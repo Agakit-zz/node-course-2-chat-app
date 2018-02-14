e7ef844a6bca68bdf52b3677804f4e39273ff3af
Agakit <agaki.tatiana@gmail.com>
2018-02-14 13:39:15 +0200

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('connected to server');
+
+  socket.emit('createMessage', {
+    from: 'Tanya',
+    text: 'Hey!its me'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('disconnected to server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+socket.emit('newMessage', {
+  from: 'Tanya',
+  text: 'See you',
+  createAt: 123123
+});
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('disconnected to server');
   });
